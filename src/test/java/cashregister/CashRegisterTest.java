c34bd4f8260957f07b42912da437c0c7cf63a0a8
CooperNiu <niuyanno.1@outlook.com>
2019-08-26 17:02:50 +0800

    finish it

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,9 +1,8 @@
 package cashregister;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
 
@@ -11,8 +10,15 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
+    	Item[] items = {new Item("test_product", 1)};
+    	Purchase purchase = new Purchase(items);
+    	MockPrinter mockedPrinter = new MockPrinter();
+    	CashRegister cashRegister = new CashRegister(mockedPrinter);
         //when
+    	cashRegister.process(purchase);
+   
         //then
+    	assertEquals("test_product\t1.0\n", mockedPrinter.gerTempText());
     }
 
     @Test
